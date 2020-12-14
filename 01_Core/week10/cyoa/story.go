81072f392ceb537ad6e6c9a41cc8e29917433a5b
LatoriMiller <lamiller@gmail.com>
2020-12-14 11:03:59 -0500

    cyoa

---
+++ 01_Core/week10/cyoa/story.go
@@ -0,0 +1,74 @@
+package cyoa
+
+import (
+	"encoding/json"
+	"io"
+	"html/template"
+	"net/http"
+)
+
+func init() {
+	tpl := template.Must(template.New("").Parse(defaultHandlerTmpl)) 
+}
+
+var tpl = *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+        <p>{{.}}</p>
+    {{end}}
+    <ul>
+    {{range .Options}}
+        <li><a href="/{{.Chapter}}"">{{.Text}}</a></li>
+    {{end}}
+    </ul>
+</body>
+</html>
+`
+func Newhandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
+
+func JsonStory(r io.Reader)(Story, error){
+	d := json.NewDecoder(r)
+	var story cyoa.story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+}
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title   		string   `json:"title"`
+	Paragraphs   	[]string `json:"story"`
+	Options 		[]Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Chapter  string `json:"arc"`
+}
+
+ 
